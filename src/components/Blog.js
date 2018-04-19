97937fa498d2f20a08a0eb7297e892d844826106
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-04-18 21:08:06 -0400

    updated blog post feed to https

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -13,7 +13,7 @@ class Blog extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40siobhanpmahoney%2F")
+    fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40siobhanpmahoney%2F")
     .then(response => response.json())
     .then(json => this.setState({
       blogPosts: json.items
