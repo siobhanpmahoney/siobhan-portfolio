770958c5cfebf2f7232292eeeaacc5b34bcaf099
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-07-06 21:39:55 -0400

    working on portfolio styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,25 +5,31 @@ import Resume from './Resume.pdf'
 
 const link = {
   width: '100px',
-  paddingTop: '1em',
-  paddingBottom: '1.5em',
-  paddingLeft: '0.75em',
-  paddingRight: '0.75em',
-  marginTop: '1em',
-  marginBottom: '1.5em',
-  marginLeft: '0.75em',
-  marginRight: '0.75em',
-  color: '#484545',
-  fontSize: '13px',
+  paddingTop: '8px',
+  paddingBottom: '8px',
+  paddingLeft: '8px',
+  paddingRight: '8px',
+  marginTop: '8px',
+  marginBottom: '8px',
+  marginLeft: '8px',
+  marginRight: '8px',
+  color: '#767171',
+  fontSize: '8px',
   textAlign: "center",
   textDecoration: "none"
+}
+
+const activestyle = {
+  color: "#8E9295",
+  boxSizing: "borderBox",
+  border: "0.25px solid #8E9295"
 }
 
 class NavBar extends React.Component {
   render() {
     return (
-      <div className="navbar" style={{color:"#484545"}}>
-        <span className="navLink"><NavLink to="/" exact style={link} activeStyle={{color:"#7fd1da", textDecoration:"underline"}}>
+      <div className="navbar" style={{color:"#6d6868"}}>
+        <span className="navLink"><NavLink to="/" exact style={link} activeStyle={activestyle}>
            Home
         </NavLink></span>
 
@@ -31,11 +37,11 @@ class NavBar extends React.Component {
              Resume
         </a></span>
 
-      <span className="navLink"><NavLink to="/portfolio" exact style={link} activeStyle={{color:"#7fd1da", textDecoration:"underline"}}>
+      <span className="navLink"><NavLink to="/portfolio" exact style={link} activeStyle={activestyle}>
            Portfolio
         </NavLink></span>
 
-      <span className="navLink"><NavLink to="/blog" exact style={link} activeStyle={{color:"#7fd1da", textDecoration:"underline"}}>
+      <span className="navLink"><NavLink to="/blog" exact style={link} activeStyle={activestyle}>
            Blog
         </NavLink></span>
 
