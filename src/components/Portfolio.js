a4692ee7012d806316e00f3e9cf333ef1857c7b7
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-07-08 15:13:02 -0400

    before refactoring portfolio proj code

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,38 @@ import captureScreenshot from './screenshots/capture-screenshot.jpg'
 import chatsterScreenshot from './screenshots/chatster-screenshot.jpg'
 import newswireScreenshot from './screenshots/newswire-screenshot.jpg'
 
+
+const projects = [
+  {
+    title: "Capture",
+    screenshot: './screenshots/capture-screenshot.jpg',
+    stack: "React, React-Router, Redux, Thunk, CSS, JWT Auth, Ruby on Rails, PostgreSQL",
+    description: "Job search resource and application tracker equipped with searchable listings and tools for organizing relevant notes and research, including a digital notepad, relevant press and news feeds, and in-app bookmarking tool.",
+    siteLink: "https://capture-jobs.herokuapp.com/login",
+    demoLink: "https://www.youtube.com/watch?v=LtX2sFyvuFA&t=1s",
+    githubLink: "https://github.com/siobhanpmahoney/capture-app",
+  },
+  {
+    title: "Chatster",
+    screenshot: './screenshots/chatster-screenshot.jpg',
+    stack: 'Action Cable, Redis, React, React-Router, Redux, Thunk, CSS, JWT Auth, Ruby on Rails, PostgreSQL',
+    description: 'Online chat application featuring real-time message delivery with search and filtering tools that optimize access to chats, message history, and friends',
+    siteLink: 'https://chatster-app.herokuapp.com/login',
+    demoLink: 'https://www.youtube.com/watch?v=EXL_lkLJs6M',
+    githubLink: 'https://github.com/siobhanpmahoney/chatster-app',
+  },
+  {
+    title: 'NYTimes NewsWire',
+    screenshot: './screenshots/newswire-screenshot.jpg',
+    stack: 'React, Isomorphic-Fetch, NYTimes API, CSS',
+    description: 'Single Page Application featuring <i>New York Times</i> article feed with real-time updates, curated recommendations, and bookmarking tool.',
+    siteLink: 'https://news-wire.herokuapp.com/',
+    demoLink: 'https://www.youtube.com/watch?v=14PjvsMuZkU&t=2s',
+    githubLink: 'https://github.com/siobhanpmahoney/newswire-app', 
+  }
+]
+
+
 class Portfolio extends React.Component {
   render() {
     return (
